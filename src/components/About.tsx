eb2167624ade1d048b6a613b4e09870661223577
Ludvig Almvång <lual@pop-os.localdomain>
2024-04-01 19:06:03 +0200

    more progress

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import img6 from '../assets/images/restaurant chef B.jpg';
-import img7 from '../assets/images/Mario and Adrian A.jpg';
+import img7 from '../assets/images/Mario and Adrian A_cropped.jpg';
 
 const About = () => {
   return (
@@ -12,9 +12,9 @@ const About = () => {
           </div>
           <div className="max-h-[207px] w-[375px]"><p className="font-karla text-lg text-justify font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu finibus  ligula. Phasellus eget venenatis lacus. Nam sed sodales lectus. In et  varius dolor, non consectetur risus. Fusce commodo mattis urna sit amet  scelerisque. Mauris ut maximus ligula. In semper ullamcorper ante ac  ultrices. Proin dui.</p></div>
       </div>
-      <div className="flex">
-        <img src={img6} alt="Img6" className="h-[350px] w-[250px] object-cover object-center border border-[#333333] drop-shadow"/>
-        <img src={img7} alt="Img7" className="h-[350px] w-[250px] object-cover object-center border border-[#333333] drop-shadow"/>
+      <div className="grid w-[410px] h-[444px]">
+        <img src={img6} alt="Img6" className="h-[350px] w-[250px] object-cover object-center border border-[#333333] drop-shadow [grid-row:1] [grid-column:1] [justify-self:start] self-end"/>
+        <img src={img7} alt="Img7" className="h-[350px] w-[250px] object-cover object-center border border-[#333333] drop-shadow [grid-row:1] [grid-column:1] [justify-self:end] self-start"/>
       </div>
     </div>
   </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,31 @@ import img8 from '../assets/images/logo-footer.png';
 
 const Footer = () => {
   return (
-    <div className="grid min-h-[500px] text-black justify-items-center items-center bg-[rgb(237,239,238)]">
+    <footer className="grid min-h-[500px] text-black justify-items-center items-center bg-[rgb(237,239,238)]">
       <div className="grid min-h-[500px] lg:grid-cols-4 lg:w-[872px] text-black justify-items-center items-center">
-        <img src={img8} alt="Img8" className="h-[250px] w-[150px] object-contain"/>
-        <p>Footer section 2</p>
-        <p>Footer section 3</p>
-        <p>Footer section 4</p>
+        <img src={img8} alt="Img8" className="h-[250px] w-[150px] object-contain lg:[justify-self:start]"/>
+        <ul className="grid">
+          <li className="font-bold">Doormat Navigation</li>
+          <li><a href="#home" className="hover:underline">Home</a></li>
+          <li><a href="#about" className="hover:underline">About</a></li>
+          <li><a href="#menu" className="hover:underline">Menu</a></li>
+          <li><a href="#reservations" className="hover:underline">Reservations</a></li>
+          <li><a href="#login" className="hover:underline">Login</a></li>
+        </ul>
+        <ul className="grid">
+          <li className="font-bold">Contact</li>
+          <li>Address</li>
+          <li><a href="#phone" className="hover:underline">Phone number</a></li>
+          <li><a href="#email" className="hover:underline">Email</a></li>
+        </ul>
+        <ul className="grid">
+          <li className="font-bold">Social Media Links</li>
+          <li><a href="#facebook" className="hover:underline">Facebook</a></li>
+          <li><a href="#instagram" className="hover:underline">Instagram</a></li>
+          <li><a href="#linkedin" className="hover:underline">Linkedin</a></li>
+        </ul>
       </div>
-  </div>
+  </footer>
   );
 };
 
